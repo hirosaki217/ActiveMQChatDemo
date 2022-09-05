dbeb69fa375a86afc756eb9a2c6b7919dbfcf153
Hieu <hirosaki217@example.com>
2022-09-05 14:49:57 +0700

    x

--- src/main/java/app/ChatApplication.java
+++ src/main/java/app/ChatApplication.java
@@ -2,6 +2,16 @@ package app;
 
 public class ChatApplication {
 	public static void main(String[] args) throws Exception{
-		new MyApp();
+		 java.awt.EventQueue.invokeLater(new Runnable() {
+	          public void run() {
+	        	  try {
+					new MyApp().setVisible(true);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				};
+	          }
+	    });
+		
 	}
 }
--- src/main/java/app/MyApp.java
+++ src/main/java/app/MyApp.java
@@ -31,7 +31,7 @@ import com.google.gson.Gson;
 
 import data.User;
 
-public class MyApp extends JFrame {
+public class MyApp extends JFrame implements ActionListener{
 	private JTextArea textArea;
 	private JTextField username;
 	private JTextField password;
@@ -41,8 +41,86 @@ public class MyApp extends JFrame {
 	private MessageProducer producer;
 	private MessageConsumer receiver;
 	private User user;
+	private JButton b;
 
 	MyApp() throws Exception {
+		
+		BasicConfigurator.configure();
+		// config environment for JNDI
+		Properties settings = new Properties();
+		settings.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+		settings.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
+		// create context
+		Context ctx = new InitialContext(settings);
+		// lookup JMS connection factory
+		ConnectionFactory factory = (ConnectionFactory) ctx.lookup("ConnectionFactory");
+		// lookup destination. (If not exist-->ActiveMQ create once)
+		Destination destination = (Destination) ctx.lookup("dynamicQueues/thanthidet");
+		// get connection using credential
+		Connection con = factory.createConnection("Ty", "123");
+		// connect to MOM
+		
+		// create session
+		session = con.createSession(/* transaction */false, /* ACK */Session.AUTO_ACKNOWLEDGE);
+		// create producer
+		producer = session.createProducer(destination);
+		// tạo consumer
+		receiver = session.createConsumer(destination);
+		
+		// create text message
+//				Message msg = session.createTextMessage("hello mesage from ActiveMQ");
+//				producer.send(msg);
+//				
+//				Message obj = session.createTextMessage("{"
+//						+ "name: 'hieu'"
+//						+ "}");
+//				producer.send(obj);
+//				Person p = new Person(1001, "Thân Thị Đẹt", new Date());
+//				
+//				
+//				msg = session.createTextMessage(gson.toJson(p));
+//				producer.send(msg);
+
+		// Cho receiver lắng nghe trên queue, chừng có message thì notify - async
+
+		receiver.setMessageListener(new MessageListener() {
+
+			// có message đến queue, phương thức này được thực thi
+			public void onMessage(Message msg) {// msg là message nhận được
+				try {
+					if (msg instanceof TextMessage) {
+						TextMessage tm = (TextMessage) msg;
+						String txt = tm.getText();
+						System.out.println("Nhận được " + txt);
+						msg.acknowledge();// gửi tín hiệu ack
+					} else if (msg instanceof ObjectMessage) {
+						ObjectMessage om = (ObjectMessage) msg;
+
+						User user = gson.fromJson(om.getObject().toString(), User.class);
+						String msg1 = user.getName() + ": " + user.getMessage();
+						System.out.println(msg1);
+						String txt = textArea.getText();
+						textArea.setText(txt + "\n" + msg1);
+					}
+					// others message type....
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		this.setLayout(new BorderLayout());
 		textArea = new JTextArea();
 		textArea.setEditable(false);
@@ -68,39 +146,12 @@ public class MyApp extends JFrame {
 		textField = new JTextField();
 		pBottom.add(textField, BorderLayout.CENTER);
 
-		JButton b = new JButton(new ImageIcon());
+		b = new JButton(new ImageIcon());
 		b.setText("Send");
+		b.addActionListener(this);
 		gson = new Gson();
 		user = new User("Guest" + new Random().nextInt() % 100, null);
-		b.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-
-				String u = username.getText().trim();
-				String p = password.getText().trim();
-				String msg = textField.getText().trim();
-//				if (u.length() > 0 && p.length() > 0 && msg.length() > 0) {
-
-					
-					user.setMessage(msg);
-					Message obj1;
-					
-						try {
-							obj1 = session.createObjectMessage(gson.toJson(user));
-							producer.send(obj1);
-//							String txt = textArea.getText();
-//							textArea.setText(txt + "\n" + user.getMessage());
-						} catch (Exception e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-	
-
-//				}
-					
-
-			}
-		});
+		
 
 		b.setBounds(100, 100, 100, 40);
 		pBottom.add(b, BorderLayout.EAST);
@@ -111,73 +162,40 @@ public class MyApp extends JFrame {
 		this.setSize(300, 400);
 
 		// config environment for JMS
-		BasicConfigurator.configure();
-		// config environment for JNDI
-		Properties settings = new Properties();
-		settings.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
-		settings.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
-		// create context
-		Context ctx = new InitialContext(settings);
-		// lookup JMS connection factory
-		ConnectionFactory factory = (ConnectionFactory) ctx.lookup("ConnectionFactory");
-		// lookup destination. (If not exist-->ActiveMQ create once)
-		Destination destination = (Destination) ctx.lookup("dynamicQueues/thanthidet");
-		// get connection using credential
-		Connection con = factory.createConnection("admin", "admin");
-		// connect to MOM
 		con.start();
-		// create session
-		session = con.createSession(/* transaction */false, /* ACK */Session.AUTO_ACKNOWLEDGE);
-		// create producer
-		producer = session.createProducer(destination);
-		// tạo consumer
-		receiver = session.createConsumer(destination);
-		// create text message
-//				Message msg = session.createTextMessage("hello mesage from ActiveMQ");
-//				producer.send(msg);
-//				
-//				Message obj = session.createTextMessage("{"
-//						+ "name: 'hieu'"
-//						+ "}");
-//				producer.send(obj);
-//				Person p = new Person(1001, "Thân Thị Đẹt", new Date());
-//				
-		final Gson gson = new Gson();
-//				
-//				msg = session.createTextMessage(gson.toJson(p));
-//				producer.send(msg);
-
-		// Cho receiver lắng nghe trên queue, chừng có message thì notify - async
 		System.out.println(user.getName() + " was listened on queue...");
-		receiver.setMessageListener(new MessageListener() {
-
-			// có message đến queue, phương thức này được thực thi
-			public void onMessage(Message msg) {// msg là message nhận được
-				try {
-					if (msg instanceof TextMessage) {
-						TextMessage tm = (TextMessage) msg;
-						String txt = tm.getText();
-						System.out.println("Nhận được " + txt);
-						msg.acknowledge();// gửi tín hiệu ack
-					} else if (msg instanceof ObjectMessage) {
-						ObjectMessage om = (ObjectMessage) msg;
-
-						User user = gson.fromJson(om.getObject().toString(), User.class);
-						String msg1 = user.getName() + ": " + user.getMessage();
-						System.out.println();
-						String txt = textArea.getText();
-						textArea.setText(txt + "\n" + msg1);
-					}
-					// others message type....
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-
-		this.setVisible(true);
+	
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		Object o = e.getSource();
+		if(o == b) {
+			
+			String u = username.getText().trim();
+			String p = password.getText().trim();
+			String msg = textField.getText().trim();
+//			if (u.length() > 0 && p.length() > 0 && msg.length() > 0) {
+
+				
+				user.setMessage(msg);
+				System.out.println(msg);
+				Message obj1;
+				
+					try {
+						obj1 = session.createObjectMessage(gson.toJson(user));
+						producer.send(obj1);
+						String txt = textArea.getText();
+						textArea.setText(txt + "\n" + msg);
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+
+//			}
+		}
 	}
 
 }
